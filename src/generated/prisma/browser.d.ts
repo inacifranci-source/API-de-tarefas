e181dbf8e2bb0c31d6d70b35aa76245a8c2a062c
inacifranci-source <inacifranci@gmail.com>
2026-09-08 23:01:38 +0100

    atualizar pasta generate

---
+++ src/generated/prisma/browser.d.ts
@@ -0,0 +1,15 @@
+import * as Prisma from './internal/prismaNamespaceBrowser.js';
+export { Prisma };
+export * as $Enums from './enums.js';
+export * from './enums.js';
+/**
+ * Model Categoria
+ *
+ */
+export type Categoria = Prisma.CategoriaModel;
+/**
+ * Model Tarefa
+ *
+ */
+export type Tarefa = Prisma.TarefaModel;
+//# sourceMappingURL=browser.d.ts.map
---
+++ src/generated/prisma/models.d.ts
@@ -0,0 +1,4 @@
+export type * from './models/Categoria.js';
+export type * from './models/Tarefa.js';
+export type * from './commonInputTypes.js';
+//# sourceMappingURL=models.d.ts.map
